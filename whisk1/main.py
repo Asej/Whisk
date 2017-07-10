38f9f0fa4f4cf0905b9c01feecf0c6cf350ae13e
Student <Student@STEM162.jcsu.edu>
2017-07-10 13:54:12 -0400

    tried to fix main py

--- whisk1/main.py
+++ whisk1/main.py
@@ -1,10 +1,15 @@
-
+import json
+import jinja2
+import os
 import webapp2
 
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        template = jinja_environment.get_template('pages/first.html')
+        self.response.write('pages/first.html')
 class ResultHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
