21a00dcfd711a67e4fb60eba6cb393232fcf9294
Student <Student@STEM162.jcsu.edu>
2017-07-10 20:55:27 -0400

    did something

--- whisk1/main.py
+++ whisk1/main.py
@@ -3,6 +3,7 @@ import jinja2
 import os
 import webapp2
 
+
 jinja_environment = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -13,7 +14,8 @@ class MainHandler(webapp2.RequestHandler):
 class ResultHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('pages/results.html')
+
         self.response.out.write(template.render())
 app = webapp2.WSGIApplication([
     ('/', MainHandler),('/results.html', ResultHandler),
-], debug=True)
+])
