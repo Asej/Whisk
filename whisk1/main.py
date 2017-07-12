8cabc99ff8b6f0d721c8ba42432565bb8d066efc
Student <Student@STEM162.jcsu.edu>
2017-07-12 15:57:27 -0400

    Finished back end project

--- whisk1/main.py
+++ whisk1/main.py
@@ -14,28 +14,39 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('pages/first.html')
         self.response.out.write(template.render())
 class ResultHandler(webapp2.RequestHandler):
-    def post(self):
+
+    def get(self):
         base_url = "http://www.recipepuppy.com/api/?"
-        url_params = {'i': self.request.get("answer"), 'q':'','p':3}
+        url_params = {'i': self.request.get("ingredients"), 'q':'','p':3}
         response = urllib.urlopen(base_url + urllib.urlencode(url_params)).read()
         parsed_dictionary= json.loads(response)
         gif_url = parsed_dictionary['results']
-        y=0
-        for y in xrange(5):
-            name = parsed_dictionary['results'][y]['title']
-            self.response.write(name +" "+"<br>")
-            ingredients = parsed_dictionary['results'][y]['ingredients']
-            self.response.write(ingredients+" "+ "<br>")
-            link = parsed_dictionary['results'][y]['href']
-            self.response.write(link+" "+"<br>")
-            picture = parsed_dictionary['results'][y]['thumbnail']
-            self.response.write(picture+" "+"<br>")
+        food={'name':[],"ingredients":[],'picture':[],'link':[] }
 
+        for y in xrange(10):
+             name = parsed_dictionary['results'][y]['title']
+             food['name'].append(name)
+        #     self.response.write(name +" "+"<br>")
+             ingredients = parsed_dictionary['results'][y]['ingredients']
+             food['ingredients'].append(ingredients)
+        #     self.response.write(ingredients+" "+ "<br>")
+             link = parsed_dictionary['results'][y]['href']
+             food['link'].append(link)
+        #     self.response.write(link+" "+"<br>")
+             picture = parsed_dictionary['results'][y]['thumbnail']
+             food['picture'].append(picture)
+        #     self.response.write(picture+" "+"<br>")
+
+
+        template = jinja_environment.get_template('pages/results.html')#change html file for template
+        self.response.out.write(template.render(food))
+        template = jinja_environment.get_template('pages/button.html')#change html file for template
+        self.response.out.write(template.render())
             #template = jinja_environment.get_template('pages/results.html')
             #my_title=gif_url["title"]
                 ####    my_pict=my_dict[results][i]["thumbnail"]
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),('/results.html', ResultHandler),
+    ('/', MainHandler),('/results', ResultHandler),
 ])
