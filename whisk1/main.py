b3f6649392880a0e849fd1b70439ea3e151d4a0d
Student <Student@STEM162.jcsu.edu>
2017-07-12 11:41:42 -0400

    got something to work

--- whisk1/main.py
+++ whisk1/main.py
@@ -14,15 +14,28 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('pages/first.html')
         self.response.out.write(template.render())
 class ResultHandler(webapp2.RequestHandler):
-        def get(self):
-            base_url = "http://www.recipepuppy.com/api/?"
-            url_params = {'q': self.request.get("answer")}
-            giphy_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
-            giphy_json_content = giphy_data_source.read()
-            parsed_giphy_dictionary = json.loads(giphy_json_content)
-            gif_url = parsed_giphy_dictionary['data'][0]['images']['original']['url']
-            template = jinja_environment.get_template('pages/results.html')
-            self.response.out.write(template.render())
+    def post(self):
+        base_url = "http://www.recipepuppy.com/api/?"
+        url_params = {'i': self.request.get("answer"), 'q':'','p':3}
+        response = urllib.urlopen(base_url + urllib.urlencode(url_params)).read()
+        parsed_dictionary= json.loads(response)
+        gif_url = parsed_dictionary['results']
+        y=0
+        for y in xrange(5):
+            name = parsed_dictionary['results'][y]['title']
+            self.response.write(name +" "+"<br>")
+            ingredients = parsed_dictionary['results'][y]['ingredients']
+            self.response.write(ingredients+" "+ "<br>")
+            link = parsed_dictionary['results'][y]['href']
+            self.response.write(link+" "+"<br>")
+            picture = parsed_dictionary['results'][y]['thumbnail']
+            self.response.write(picture+" "+"<br>")
+
+            #template = jinja_environment.get_template('pages/results.html')
+            #my_title=gif_url["title"]
+                ####    my_pict=my_dict[results][i]["thumbnail"]
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),('/results.html', ResultHandler),
 ])
